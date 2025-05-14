dd3aa9c72b863fe703fd4217c1b200cdf56c9779
HiepH <youremail@example.com>
2025-05-14 15:43:09 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.demo {
     requires java.logging;
     requires java.net.http;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
 //    requires spring.boot;
 //    requires spring.boot.autoconfigure; // nếu bạn dùng HttpClient thay vì HttpURLConnection
 
